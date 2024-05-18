73cacae1cd5bcadd323f9bffe6c6285e8054fabc
goMun3 <161525109+goMun3@users.noreply.github.com>
2024-05-19 00:05:03 +0300

    2

--- video-swap-new/video-swap-new-ublock-origin.js
+++ video-swap-new/video-swap-new-ublock-origin.js
@@ -77,7 +77,7 @@ twitch-videoad.js text/javascript
                 if (e.data.key == 'UboShowAdBanner') {
                     var adDiv = getAdDiv();
                     if (adDiv != null) {
-                        adDiv.P.textContent = 'Blocking' + (e.data.isMidroll ? ' midroll' : '') + ' ads';
+                        adDiv.P.textContent = 'Blocking' + (e.data.isMidroll ? '2' : '') + ' ads';
                         if (OPT_SHOW_AD_BANNER) {
                             adDiv.style.display = 'block';
                         }
